71bbac7976b0c0206427f0d66653bb761e533c49
Kirti Pawar <Kirti.Pawar@lntinfotech.com>
2022-09-22 17:15:33 +0530

    Added JWT Authentication API

---
+++ User.cs
@@ -0,0 +1,9 @@
+namespace WebAPIProject
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash{ get; set; } 
+        public byte[] PasswordSalt { get; set; } 
+    }
+}
---
+++ UserDto.cs
@@ -0,0 +1,10 @@
+namespace WebAPIProject
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        
+        public string Password { get; set; } = string.Empty;
+    }
+}
+
